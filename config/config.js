7ec630aa7fd1a6726ac4a41fddd0012826fb3544
Pierre-Richard Pascal <89923424+Pascal95@users.noreply.github.com>
2024-09-12 15:36:00 +0200

    arggg

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
+require('dotenv').config();  // Charge les variables d'environnement à partir du fichier .env
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME || 'upermed',  // Par défaut 'root' si non défini
+    password: process.env.DB_PASSWORD || 'hardpassword',    // Par défaut null si non défini
+    database: process.env.DB_DATABASE || 'UperMed',
+    host: process.env.DB_HOST || '51.178.82.36',
     dialect: 'mysql'
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME || 'upermed',  // Par défaut 'root' si non défini
+    password: process.env.DB_PASSWORD || 'hardpassword',    // Par défaut null si non défini
+    database: process.env.DB_DATABASE || 'UperMed',
+    host: process.env.DB_HOST || '51.178.82.36',
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME || 'upermed',  // Par défaut 'root' si non défini
+    password: process.env.DB_PASSWORD || 'hardpassword',    // Par défaut null si non défini
+    database: process.env.DB_DATABASE || 'UperMed',
+    host: process.env.DB_HOST || '51.178.82.36',
     dialect: 'mysql'
   }
 };
