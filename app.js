d3ffca159c92eca4b3df2e12c90a8465b88902c3
Pierre-Richard Pascal <89923424+Pascal95@users.noreply.github.com>
2025-04-09 11:58:31 +0200

    cors

--- app.js
+++ app.js
@@ -18,7 +18,21 @@ const e = require('express');
 const { authenticateToken } = require('./middlewares/auth'); 
 
 const app = express();
-app.use(cors());
+const allowedOrigins = [
+  'https://www.taxis-med.fr',
+  'http://localhost:3002'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 
 let keyporc = "";
